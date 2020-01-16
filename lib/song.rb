0cefffac9ed28720bc07bfb32e5630d33d152e5e
Amy Fong <amyhfong@gmail.com>
2020-01-16 21:01:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,10 +13,8 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
+    @@genres << genre
     @@count += 1
-    if !GENRE.include?(genre)
-      GENRE << genre
-    end
 
   end
 
