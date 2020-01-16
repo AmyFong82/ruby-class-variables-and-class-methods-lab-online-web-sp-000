b0cfa25c6903cf39c7affddab428780b02c1813e
Amy Fong <amyhfong@gmail.com>
2020-01-16 20:54:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,16 @@
 class Song
   attr_accessor :name, :artist, :genre
+  
   @@song_count = 0
 
 
   def self.count
     @@song_count
   end
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
 
 end
