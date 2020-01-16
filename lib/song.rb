45a688ae31ea26224972e5608efb807298415a90
Amy Fong <amyhfong@gmail.com>
2020-01-16 21:35:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,6 +15,11 @@ class Song
   end
 
   def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count.key?(genre)
+        genre_count[genre] += 1
+    end
   end
 
 
